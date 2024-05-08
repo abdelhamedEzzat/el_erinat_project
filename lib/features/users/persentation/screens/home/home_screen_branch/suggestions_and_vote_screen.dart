462fdafdba54632161f8b26a4cb47acbbf0c13e7
abdelhamid <amabdo275@gmail.com>
2024-05-08 13:59:57 +0300

    finished button suggetion text

--- lib/features/users/persentation/screens/home/home_screen_branch/suggestions_and_vote_screen.dart
+++ lib/features/users/persentation/screens/home/home_screen_branch/suggestions_and_vote_screen.dart
@@ -1,7 +1,7 @@
-import 'package:el_erinat/core/config/color_manger.dart';
 import 'package:el_erinat/core/const_strings/manage_strings.dart';
 import 'package:el_erinat/core/helpers/back_ground_and_app_bar_and_dynamic_body.dart';
 import 'package:el_erinat/core/route/route_strings.dart';
+import 'package:el_erinat/features/users/persentation/widgets/home_widget/suggestions_and_vote/buttom_suggetion_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -52,81 +52,3 @@ class SuggestionsandVoteScreen extends StatelessWidget {
         ));
   }
 }
-
-class BottonSuggetionText extends StatelessWidget {
-  const BottonSuggetionText({
-    super.key,
-    required this.chooseText,
-    this.onTap,
-  });
-  final String chooseText;
-  final void Function()? onTap;
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        width: 200.w,
-        height: 130.h,
-        decoration: BoxDecoration(
-          color: ColorManger.logoColor.withOpacity(0.5),
-          borderRadius: BorderRadius.all(Radius.circular(8.h)),
-        ),
-        padding: EdgeInsets.all(25.h),
-        child: Center(
-          child: Text(
-            chooseText,
-            style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                color: Colors.white,
-                fontSize: 18.h,
-                fontWeight: FontWeight.bold),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-
-
-
-// Positioned.fill(
-//           top: 120.h,
-//           child: SingleChildScrollView(
-//             child: Container(
-//               padding: EdgeInsets.all(20.w),
-//               child: Column(
-//                 children: [
-//                   Container(
-//                     width: MediaQuery.of(context).size.width,
-//                     decoration: BoxDecoration(
-//                       color: ColorManger.logoColor.withOpacity(0.5),
-//                       borderRadius: BorderRadius.all(Radius.circular(8.h)),
-//                     ),
-//                     padding: EdgeInsets.all(25.h),
-//                     child: Column(
-//                       crossAxisAlignment: CrossAxisAlignment.end,
-//                       children: [
-//                         const VoteTitle(),
-//                         SizedBox(
-//                           height: 15.h,
-//                         ),
-//                         const AnimatedVoteContainer(),
-//                         SizedBox(
-//                           height: 15.h,
-//                         ),
-//                         const AnimatedVoteContainer(
-//                           textOfVote: "رفض",
-//                         ),
-//                         SizedBox(
-//                           height: 15.h,
-//                         ),
-//                         const VoteNumberAndDate(),
-//                       ],
-//                     ),
-//                   )
-//                 ],
-//               ),
-//             ),
-//           ),
-//         )
